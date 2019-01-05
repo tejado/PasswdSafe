ccbc11b6b871cf001f16f6b9bc6f347b0a3e8f20
Jeff Harris <jefftharris@gmail.com>
2019-01-05 14:36:35 -0500

    Update OneDrive support to use latest MS Graph API

---
+++ sync/src/main/java/com/jefftharris/passwdsafe/sync/onedrive/Constants.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (©) 2018 Jeff Harris <jefftharris@gmail.com>
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+package com.jefftharris.passwdsafe.sync.onedrive;
+
+
+/**
+ * Constants for accessing OneDrive
+ */
+public class Constants
+{
+    public static final String CLIENT_ID =
+            "0411d3ff-3747-43cf-9e3d-9f194a84a6c0";
+
+    public static final String[] SCOPES =
+            { "Files.ReadWrite", "User.Read" };
+}
